b11d0d24a09a0cc38ce77926cee4941e7745f6c4
Randoro <anton.arnoldsson@hotmail.com>
2015-12-30 16:00:52 +0100

    trying again

--- Source/HyperSpeed/Lane.cpp
+++ Source/HyperSpeed/Lane.cpp
@@ -11,24 +11,25 @@ ALane::ALane()
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/Geometry/Meshes/1M_Cube.1M_Cube"));
 	// Create the mesh component
 	LaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LaneMesh"));
-	//RootComponent = LaneMeshComponent;
+	RootComponent = LaneMeshComponent;
 	LaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
 	LaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	LaneMeshComponent->AddLocalTransform(FTransform(FVector(0.0f, 10.0f, 0.0f)));
+	LaneMeshComponent->AddWorldTransform(FTransform(FVector(0.0f, 0.0f, 0.0f)));
 
 	// Create the left mesh component
 	LeftLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftLaneMesh"));
 	//RootComponent = LeftLaneMeshComponent;
 	LeftLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
 	LeftLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	LeftLaneMeshComponent->AddLocalTransform(FTransform(FVector(0.0f, -10.0f, 0.0f)));
+	LeftLaneMeshComponent->SetWorldTransform(LaneMeshComponent->GetComponentTransform());
+	LeftLaneMeshComponent->AddWorldTransform(FTransform(FVector(0.0f, -10.0f, 0.0f)));
 
-	// Create the right mesh component
-	RightLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightLaneMesh"));
-	//RootComponent = RightLaneMeshComponent;
-	RightLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
-	RightLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	RightLaneMeshComponent->AddLocalTransform(FTransform(FVector(0.0f, 10.0f, 0.0f)));
+	//// Create the right mesh component
+	//RightLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightLaneMesh"));
+	////RootComponent = RightLaneMeshComponent;
+	//RightLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	//RightLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
+	//RightLaneMeshComponent->AddWorldTransform(FTransform(FVector(0.0f, 10.0f, 0.0f)));
 
 
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
