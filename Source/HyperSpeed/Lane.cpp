d08422705b521ea4bed3d82ed6d24bb59ac7210a
Randoro <anton.arnoldsson@hotmail.com>
2015-12-30 15:27:41 +0100

    added lane

---
+++ Source/HyperSpeed/Lane.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HyperSpeed.h"
+#include "Lane.h"
+
+
+// Sets default values
+ALane::ALane()
+{
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/Geometry/Meshes/1M_Cube.1M_Cube"));
+	// Create the mesh component
+	LaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LaneMesh"));
+	RootComponent = LaneMeshComponent;
+	LaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	LaneMeshComponent->SetStaticMesh(ShipMesh.Object);
+
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALane::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALane::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/HyperSpeed/Lane.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Lane.generated.h"
+
+UCLASS()
+class HYPERSPEED_API ALane : public AActor
+{
+	GENERATED_BODY()
+
+	/* The mesh component */
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* LaneMeshComponent;
+	
+public:	
+	// Sets default values for this actor's properties
+	ALane();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
