a37f3778a90bee5b407cda8be4cd2dcfdab76f76
Randoro <anton.arnoldsson@hotmail.com>
2016-01-03 03:10:38 +0100

    creating lanes

--- Source/HyperSpeed/Lane.cpp
+++ Source/HyperSpeed/Lane.cpp
@@ -8,33 +8,13 @@
 ALane::ALane()
 {
 	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComp"));
-
-	RootComponent = SceneComponent;
-
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/Geometry/Meshes/1M_Cube.1M_Cube"));
-	// Create the mesh component
 	LaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LaneMesh"));
-	LaneMeshComponent->AttachTo(RootComponent);
-	LaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
-	LaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	LaneMeshComponent->AddLocalTransform(FTransform(FVector(100.0f, 0.0f, 0.0f)));
-
-	// Create the left mesh component
 	LeftLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftLaneMesh"));
-	//RootComponent = LeftLaneMeshComponent;
-	LeftLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
-	LeftLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	LeftLaneMeshComponent->AttachTo(RootComponent);
-	//SetupOrientation();
-	//LeftLaneMeshComponent->SetRelativeLocation(LaneMeshComponent->RelativeLocation);
-	//LeftLaneMeshComponent->AddWorldTransform(FTransform(FVector(0.0f, -10.0f, 0.0f)));
+	RightLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightLaneMesh"));
 
-	//// Create the right mesh component
-	//RightLaneMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightLaneMesh"));
-	////RootComponent = RightLaneMeshComponent;
-	//RightLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
-	//RightLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
-	//RightLaneMeshComponent->AddWorldTransform(FTransform(FVector(0.0f, 10.0f, 0.0f)));
+	InitializeComponents();
+
+	SetupOrientation();
 
 
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -42,20 +22,60 @@ ALane::ALane()
 
 }
 
+void ALane::InitializeComponents()
+{
+	//Create static texture
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/Geometry/Meshes/1M_Cube.1M_Cube"));
+
+	//SceneComponent
+	RootComponent = SceneComponent;
+
+	//Middle
+	LaneMeshComponent->AttachTo(RootComponent);
+	LaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	LaneMeshComponent->SetStaticMesh(ShipMesh.Object);
+
+	//Left
+	LeftLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	LeftLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
+	LeftLaneMeshComponent->AttachTo(RootComponent);
+
+	//Right
+	RightLaneMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	RightLaneMeshComponent->SetStaticMesh(ShipMesh.Object);
+	RightLaneMeshComponent->AttachTo(RootComponent);
+
+
+
+}
+
 void ALane::SetupOrientation()
 {
-	FVector Position;
-	Position.X = 0.0f;
-	Position.Y = 0.0f;
-	Position.Z = 1000.0f;
 
-	FRotator Rotation;
-	Rotation.Yaw = 0.0f;
-	Rotation.Pitch = 0.0f;
-	Rotation.Roll = 0.0f;
+	FVector LaneScale;
+	LaneScale.X = 1.0f;
+	LaneScale.Y = 12.0f;
+	LaneScale.Z = 1.0f;
 
-	LeftLaneMeshComponent->SetMobility(EComponentMobility::Movable);
-	LeftLaneMeshComponent->SetWorldLocationAndRotation(Position, Rotation);
+
+
+	//Position
+	FVector LeftLanePosition;
+	LeftLanePosition.X = 0.0f;
+	LeftLanePosition.Y = 100.0f;
+	LeftLanePosition.Z = 10.0f;
+
+	FVector RightLanePosition;
+	RightLanePosition.X = 0.0f;
+	RightLanePosition.Y = -100.0f;
+	RightLanePosition.Z = 10.0f;
+
+	//Scale
+	LaneMeshComponent->SetRelativeScale3D(LaneScale);
+	//Position
+	LeftLaneMeshComponent->AddLocalTransform(FTransform(LeftLanePosition));
+	RightLaneMeshComponent->AddLocalTransform(FTransform(RightLanePosition));
+
 }
 
 // Called when the game starts or when spawned
--- Source/HyperSpeed/Lane.h
+++ Source/HyperSpeed/Lane.h
@@ -38,6 +38,8 @@ public:
 
 	void SetupOrientation();
 
+	void InitializeComponents();
+
 	
 	
 };
